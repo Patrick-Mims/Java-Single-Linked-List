fec0bfdb816d33de41924e46f4bd194b3ec0e832
weatherpub <patrick@bayareamedia.net>
2023-07-19 18:53:00 -0700

    latest

---
+++ src/main/java/com/sfsu/app/SingleLinkedList.java
@@ -0,0 +1,62 @@
+package com.sfsu.app;
+
+public class SingleLinkedList {
+
+    private static class Node {
+       String item;
+       Node node;
+
+       public Node() {
+          this.node = null;
+          this.item = null;
+       }
+       public Node(Node newNode, String newItem) {
+            this.item = newItem;
+            this.node = newNode;
+        }
+    }
+
+    private Node head;
+
+    SingleLinkedList() {
+        head = null;
+    }
+
+    public void insertBeginning(String item, int count) {
+        head = new Node(head, item);
+    }
+
+    public void displayList() {
+
+        Node position = head;
+
+        while(position != null) {
+            System.out.println("Company => " + position.item);
+            position = position.node;
+        }
+    }
+
+    public boolean deleteNode() {
+       if(head != null)
+       {
+           head = head.node;
+           return true;
+       }
+
+       return false;
+    }
+
+    public int size() {
+        int count = 0;
+
+        Node position = head;
+
+        while(position != null)
+        {
+            count++;
+            position = position.node;
+        }
+
+        return count;
+    }
+}
